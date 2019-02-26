b63925f863e302464b847f10e6bfd7bd8866f062
Terriculum <d_z_omba@hotmail.com>
2019-02-26 23:42:17 +0100

    Move results to results folder

--- olx_ba_scrapper_v1.py
+++ olx_ba_scrapper_v1.py
@@ -1,32 +1,26 @@
 from urllib.request import urlopen as uReq
 from bs4 import BeautifulSoup as soup
-import os
+from datetime import datetime
 
 i = 0
-#inp_1 = input("Unesite prvi pojam pretrage: ")
-#inp_2 = input("Unesite drugi pojam pretrage: ")
-inp_3 = input()
-inp_4 = list(inp_3)
-inp_5 = "".join(inp_4[:-1])
-while i < 20:
-    i += 1
-    my_url = "https://www.{}{}".format(inp_5, i)
-    print(my_url)
-    #my_url = "https://www.olx.ba/pretraga?trazilica=+{}+{}&kategorija=31&stranica=".format(inp_1, inp_2) + str(i)
-    uClient = uReq(my_url)
-    page_html = uClient.read()
-    uClient.close()
-    page_soup = soup(page_html, "html.parser")
+inp_1 = input("Unesite prvi pojam pretrage: ")
+inp_2 = input("Unesite drugi pojam pretrage: ")
+FILE_NAME = "pik_{}_{}_{}.csv".format(inp_1, inp_2, str(datetime.now().strftime("%d-%m-%Y, %H-%M-%S")))
 
-    results = page_soup.findAll("div", {"class": "listitem artikal obicniArtikal imaHover-disabled"})
+with open("results/" + FILE_NAME, "a", encoding="utf-8") as file:
+    file.write("Naziv, Opis, Cijena, Lokacija\n")
+    while True:
+        i += 1
+        my_url = "https://www.olx.ba/pretraga?trazilica=+{}+{}&stranica={}".format(inp_1, inp_2, str(i))
+        uClient = uReq(my_url)
+        page_html = uClient.read()
+        uClient.close()
+        page_soup = soup(page_html, "html.parser")
 
-    result = results[0]
-    with open("pik_samsung_s5.csv", "a", encoding="utf-8") as samsung_s5:
+        results = page_soup.findAll("div", {"class": "listitem artikal obicniArtikal imaHover-disabled"})
+        try:
+            result = results[0]
 
-        headers = "Naziv, Opis, Cijena, Lokacija\n"
-        if os.stat('pik_samsung_s5.csv').st_size == 0:
-            samsung_s5.write(headers)
-        else:
             for result in results:
                 name_result = result.findAll("p", {"class": "na"})
                 name = name_result[0].text.replace(",", "|")
@@ -36,5 +30,6 @@ while i < 20:
                 price = price_result[0].span.text.replace(",", "|")
                 location_result = result.findAll("div", {"class": "lokacijadiv"})
                 location = location_result[0].text.replace(",", "|")
-                samsung_s5.write("{} , {} , {}, {} \n".format(name, subname, price, location))
-
+                file.write("{} , {} , {}, {} \n".format(name, subname, price, location))
+        except Exception as ex:
+            break
