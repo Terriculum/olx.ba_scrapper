509a5cbe4ebc6231f79d187606872addaf386b5b
Terriculum <d_z_omba@hotmail.com>
2019-03-01 00:16:37 +0100

    Version2

--- olx_ba_scrapper_v1.py
+++ olx_ba_scrapper_v1.py
@@ -2,13 +2,17 @@ from urllib.request import urlopen as uReq
 from bs4 import BeautifulSoup as soup
 from datetime import datetime
 
-i = 0
+
 inp_1 = input("Unesite prvi pojam pretrage: ")
 inp_2 = input("Unesite drugi pojam pretrage: ")
 FILE_NAME = "pik_{}_{}_{}.csv".format(inp_1, inp_2, str(datetime.now().strftime("%d-%m-%Y, %H-%M-%S")))
 
-with open("results/" + FILE_NAME, "a", encoding="utf-8") as file:
-    file.write("Naziv, Opis, Cijena, Lokacija\n")
+Paid = ("div", {"class": "listitem artikal obicniArtikal imaHover-disabled i"})
+Regular = ("div", {"class": "listitem artikal obicniArtikal imaHover-disabled"})
+Highlight = ("div", {"class": "listitem artikal obicniArtikal istaknin imaHover-disabled"})
+
+def scrapper(source):
+    i = 0
     while True:
         i += 1
         my_url = "https://www.olx.ba/pretraga?trazilica=+{}+{}&stranica={}".format(inp_1, inp_2, str(i))
@@ -16,11 +20,10 @@ with open("results/" + FILE_NAME, "a", encoding="utf-8") as file:
         page_html = uClient.read()
         uClient.close()
         page_soup = soup(page_html, "html.parser")
-
-        results = page_soup.findAll("div", {"class": "listitem artikal obicniArtikal imaHover-disabled"})
+        results = page_soup.findAll(source[0], source[1])
+        print(results)
         try:
             result = results[0]
-
             for result in results:
                 name_result = result.findAll("p", {"class": "na"})
                 name = name_result[0].text.replace(",", "|")
@@ -33,3 +36,9 @@ with open("results/" + FILE_NAME, "a", encoding="utf-8") as file:
                 file.write("{} , {} , {}, {} \n".format(name, subname, price, location))
         except Exception as ex:
             break
+
+with open("results/" + FILE_NAME, "a", encoding="utf-8") as file:
+    file.write("Naziv, Opis, Cijena, Lokacija\n")
+    scrapper(Paid)
+    scrapper(Regular)
+    scrapper(Highlight)
